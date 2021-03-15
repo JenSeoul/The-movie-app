8f56db3ce3ed92658a56eba2fe7fc85b3cb33d8d
JenSeoul <n.waves429@gmail.com>
2021-03-15 15:29:04 +0900

    install dotenv /api key stored in dotenv

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+require('dotenv').config()
+
+app.use('/', express.static('public'));
+app.use(express.json({limit:'1mb'}));
+app.use(express.urlencoded({extended: true}));
+
+app.get('/movie', async (req,res)=>{
+
+    const api_key =process.env.api_key
+    const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1`;
+
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    res.json(data.results)
+
+})
+
+app.get('/result/:search', async (req,res)=>{
+    const searchValue = req.params.search
+    const api_key =process.env.api_key
+
+    const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="${searchValue}`;
+
+    const response = await fetch(SEARCH_API);
+    const data = await response.json();
+
+    res.json(data.results)
+    
+})
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log('Now server is running'))
---
+++ public/main.js
@@ -0,0 +1,100 @@
+
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
+const api = 'movie/';
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const main = document.querySelector('#main');
+
+window.onload = getMovies(api)
+
+async function getMovies(url){
+    const res = await fetch(url);
+    const data = await res.json();
+    showMovies(data)
+}
+
+function showMovies(movies){
+    main.innerHTML = ''
+    
+    movies.forEach((movie)=>{
+        for(movie of movies){
+            const {title,poster_path,vote_average,overview} = movie
+            const movieEl = document.createElement('div')
+            movieEl.classList.add('movie')
+            movieEl.innerHTML = `
+            <div class="overview">
+            <h3>Overview</h3>
+             ${overview}
+            </div>
+            <img src="${IMG_PATH+poster_path}" alt="${title}">
+            <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByrate(vote_average)}">${vote_average}</span>
+            </div>
+            `
+            main.appendChild(movieEl);
+            
+            main.addEventListener('click', oneMovie);
+        }
+    })
+}
+async function oneMovie (e){
+    const div = Array.from(e.target.children)
+    const moviediv = e.target.parentElement
+    const rating = div[1].textContent
+    const movieTitle = div[0].textContent
+    const movieImage = Array.from(moviediv.children)[1]
+    const overview = Array.from(moviediv.children)[0].innerText 
+
+    main.innerHTML = ''
+    const onemovieEl = document.createElement('div')
+    onemovieEl.innerHTML = `
+    <div class="container">
+        <div class="movieposter">
+            <img src="${movieImage.src}" alt="${movieTitle}">
+            <h4>Now streaming on movieDB✨</h4>
+        </div>
+        <div class="oneoverview">
+            <h3>${movieTitle}</h3>
+            <span class="${getClassByrate(rating)}">${rating}</span>
+            <h4>Overview</h4><p>${overview.slice(8,overview.length)}</p>
+            <div class="borderbottom"></div>
+        </div>
+    </div>
+    <div class="home"><i class="fas fa-angle-down"></i><button id="backtolist" class="goback">Back to the list</button></div>
+    `
+    main.appendChild(onemovieEl);
+    document.querySelector('#backtolist').addEventListener('click', ()=>{
+        main.innerHTML = '';
+        window.location.reload();
+    });
+}
+
+function getClassByrate(rate){
+    if(rate>=8){
+        return 'green'
+    }
+    if(rate>=5 && rate<8){
+        return 'orange'
+    }if(rate<5){
+        return 'grey'
+    }
+}
+
+form.addEventListener('submit', async(e)=>{
+    e.preventDefault();
+    console.log(search.value)
+
+    const searchTerm = search.value;
+
+    if(searchTerm){
+    const res = await fetch(`result/${searchTerm}`);
+    const searchResult = await res.json()
+    showMovies(searchResult)
+    search.value = '';
+    }
+    else{
+        window.location.reload()
+    }
+})
+
