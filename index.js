697692c6bf38c5240471555bb079a162fa113936
JenSeoul <n.waves429@gmail.com>
2021-03-15 16:51:08 +0900

    change index.js

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
 require('dotenv').config()
-console.log(process.env)
 
 app.use('/', express.static('public'));
 app.use(express.json({limit:'1mb'}));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/movie', async (req,res)=>{
+app.get('/', async (req,res)=>{
 
-    const api_key =process.env.api_key
     const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1`;
 
     const response = await fetch(API_URL);
@@ -22,7 +20,6 @@ app.get('/movie', async (req,res)=>{
 
 app.get('/result/:search', async (req,res)=>{
     const searchValue = req.params.search
-    const api_key =process.env.api_key
 
     const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query="${searchValue}`;
 
--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280/';
-const api = 'movie/';
+const api = '/';
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const main = document.querySelector('#main');
