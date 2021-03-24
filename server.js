c957f096ba9f81a978667a46dee2396661d71517
Jen <n.waves429@gmail.com>
2021-03-24 17:08:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ app.use(express.json({limit:'1mb'}));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/movie', async (req,res)=>{
-    const api_key= process.env.api_key
+   try{ const api_key= process.env.api_key
     const API_URL = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${api_key}&page=1`;
 
     const response = await fetch(API_URL);
     const data = await response.json();
 
     res.json(data.results)
+      }
+   catch(err){
+    console.err(error)
+    res.json({err.message})
+   }
 
 })
 
